a1e0cf7470df2ee3eeda8145e37049ea07dd431c
HagiNicolas <148631946+HagiNicolas@users.noreply.github.com>
2024-04-25 22:07:22 +0300

    Add files via upload

---
+++ RestaurantApp/Migrations/_AddPhotoUrl.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+
+namespace RestaurantApp.Migrations
+{
+    
+    public partial class AddPhotoUrl : Migration
+    {
+        
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                schema: "RestaurantAppSchema",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                schema: "RestaurantAppSchema",
+                table: "Items");
+        }
+    }
+}
